daba210d6a09c4d8ba401cdb1312a5431f0f0d20
farmanfirnas <iamfarmanmalik@gmail.com>
2023-03-26 01:48:53 +0530

    Task model & user model built

---
+++ server/model/TaskModel.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+
+const TaskSchema=mongoose.Schema({
+        task:{type:String,required:true},
+        status:{type:String,default:"pending"},
+        assign:[
+            {type:mongoose.Types.ObjectId,ref:"user",required:true}
+        ],
+        sprint:{type:Number,required:true}
+})
+
+const TaskModel=mongoose.model("task",TaskSchema);
+
+module.exports=TaskModel
