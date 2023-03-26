948bd2a169422148783202a9d449b05f068a6718
farmanfirnas <iamfarmanmalik@gmail.com>
2023-03-26 10:32:36 +0530

    minor changes

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -6,7 +6,7 @@ export const Header=()=>{
         <Stack p="20px" direction={"row"} bgColor="rgb(9,30,66)" justifyContent="space-between" minW="100%" >
             <Heading color="white" fontSize="25px">TaskPlanner.</Heading>
             {/* <Image width={{base:"100px",md:"200px" }}src={logo}/>             */}
-        <NavLink to={"/user"}>   <Button height={{base:"40px",md:"40px"}} fontSize={{base:"17px",md:"19px"}} width="160px" bgColor="rgb(255,167,7)" color={"white"}>Sign In</Button></NavLink>
+        <NavLink to={"/"}>   <Button height={{base:"40px",md:"40px"}} fontSize={{base:"17px",md:"19px"}} width="160px" bgColor="rgb(255,167,7)" color={"white"}>Sign In</Button></NavLink>
         </Stack>
     )
 }
